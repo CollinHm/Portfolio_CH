5366ab815d3050dd0ef589bf772b6eed49ede67c
CollinHm <collinhermans7@gmail.com>
2025-04-03 11:40:42 +0200

    Copy paste from school repo

---
+++ script.js
@@ -0,0 +1,21 @@
+function goToPage() {
+    window.location.href = "Menu.html";
+}
+function goToPage2() {
+    window.location.href = "index.html";
+}
+function goToPage3() {
+    window.location.href = "About-me.html" ;
+}
+function goToPage4() {
+    window.location.href = "Menu.html";
+}
+function goToPage5() {
+    window.location.href = "Experience.html";
+}
+function goToPage6() {
+    window.location.href = "Menu.html";
+}
+function goToPage7() {
+    window.location.href = "Projects.html";
+}
